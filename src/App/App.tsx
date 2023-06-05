3ea5f8d79f171a88f084fe6f18d395d8dd19ade2
Austin Aitken <austinaitken@Austins-MacBook-Pro-2.local>
2023-06-05 17:37:21 -0230

    Added high-level file structure

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from '../components/top-level/Header/Header'
+import Body from '../components/top-level/Body/Body'
+import Footer from '../components/top-level/Footer/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
